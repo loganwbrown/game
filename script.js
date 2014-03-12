ab541576dfccec4304aa65d9c0269da681fa8e05
DPL-4 <DPL-4@DPL-4s-iMac.local>
2014-03-12 12:24:05 -0700

    created checkboard like divs

---
+++ script.js
@@ -0,0 +1,87 @@
+// Lets Create A Wrapper in jQuery's Document Ready Syntax
+// for the functions we will build out today.
+
+$(document).ready(function(){
+
+  formatBoxes();
+  odd();
+  even();
+  greeting();
+  toggler();
+});
+
+  function formatBoxes() {
+    $('.box').css({
+    'height': '150px',
+    'width': '150px',
+    'display': 'inline-block',
+    'float': 'left'
+
+  });
+}
+
+  function odd() {
+    $('.odd').css({
+    'background': 'darkred'
+  });
+}
+
+  function even() {
+    $('.even').css({
+    'background': 'black'
+  });
+}
+
+  function greeting() {
+    if(confirm("Are you going ready to play?") === true){
+      $('#main-container').css({
+        'opacity': '1'
+      });
+
+      animator();
+    }
+    else {
+      $('#main-container').css({
+        'opacity': 0
+
+      });
+    } 
+  }
+
+//   function toggler() {
+//     $('.box').click(function(){
+//       if($('div:hidden').opacity > 0) {
+//         var hiddenElements = $('div:hidden');
+//         hiddenElements.addClass('unhidden');
+//         hiddenElements.show();
+//         $(this).show();
+//       } else {
+//         var shownElements = $('div.unhidden');
+//         shownElements.removeClass('unhidden');
+//         shownElements.hide();
+//         $(this).hide();
+//       }
+//       $('hidden').toggle();
+//       $('unhidden').toggle();
+
+//       return false;
+//     })
+// }
+
+  function toggler() {
+    $('.box').click(function(){
+      $(this).toggleClass('hide');
+    });
+  }
+
+  function animator() {
+    $('h1').animate({ 
+
+      'width': '150%',
+      'opacity': '1',
+      'marginLeft': '0.6in',
+      'fontSize': '3em',
+      'borderWidth': '10px'
+
+    });
+  }
